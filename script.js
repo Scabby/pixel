30555b7d9474cb4cd2f3fea6bb964f4bf619b599
Scabby <61923737+Scabby@users.noreply.github.com>
2022-06-22 10:24:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,41 @@ function drawLine(new_x, new_y) {
 document.addEventListener("load", (e) => {
     canvas  = document.getElementsByTagName("canvas")[0]
     ctx     = canvas.getContext("2d")
+    
+    canvas.addEventListener("mousedown", (e) => {
+        drawing = true
+        x       = e.offsetX
+        y       = e.offsetY
+
+        let target = ctx.getImageData(x, y, 1, 1).data
+
+        let is_black =  target[0] < 128
+                    &&  target[1] < 128
+                    &&  target[2] < 128
+
+        if(is_black)    { color = "#000000" }
+        else            { color = "#ffffff" }
+    })
+
+    canvas.addEventListener("mousemove", (e) => {
+        if(drawing) {
+            draw_line(e.offsetX, e.offsetY)
+        }
+    })
+
+    canvas.addEventListener("mouseup", (e) => {
+        if(drawing) {
+            draw_line(e.offsetX, e.offsetY)
+            drawing = false
+        }
+    })
+    
+    canvas.addEventListener("mouseleave", (e) => {
+        if(drawing) {
+            draw_line(e.offsetX, e.offsetY)
+            drawing = false
+        }
+    })
 })
 
 
@@ -26,50 +61,3 @@ document.addEventListener("keyup", (e) => {
     e.preventDefault()
     //
 })
-
-
-
-canvas.addEventListener("mousedown", (e) => {
-    drawing = true
-    x       = e.offsetX
-    y       = e.offsetY
-    
-    let target = ctx.getImageData(x, y, 1, 1).data
-    
-    let is_black =  target[0] < 128
-                &&  target[1] < 128
-                &&  target[2] < 128
-    
-    if(is_black)    { color = "#000000" }
-    else            { color = "#ffffff" }
-})
-
-canvas.addEventListener("mousemove", (e) => {
-    if(drawing) {
-        draw_line(e.offsetX, e.offsetY)
-    }
-})
-
-document.addEventListener("mouseup", (e) => {
-    if(drawing) {
-        draw_line(e.offsetX, e.offsetY)
-    }
-    drawing = false
-})
-
-
-
-canvas.addEventListener("touchstart", (e) => {
-    e.preventDefault()
-    drawing = true
-}, { passive: false })
-
-canvas.addEventListener("touchmove", (e) => {
-    e.preventDefault()
-    draw = { x: e.layerX, y: e.layerY }
-}, { passive: false })
-
-document.addEventListener("touchend", (e) => {
-    e.preventDefault()
-    drawing = true
-}, { passive: false })
