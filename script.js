240139551041e8e65162cdabd0050b40982bb824
Scabby <61923737+Scabby@users.noreply.github.com>
2022-06-23 00:02:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const black = "#000000"
-const white = "#ffffff"
-let drawing = false
+const black         = [0, 0, 0, 255]
+const white         = [255, 255, 255, 255]
+const transparent   = [255, 255, 255, 0]
+let drawing         = false
+let shifting        = false
+
 
 
 function get_canvas_coords(x, y) {
@@ -12,31 +15,44 @@ function get_canvas_coords(x, y) {
 
 function get_pixel(x, y) {
     let coords = get_canvas_coords(x, y)
-    
+
     return ctx.getImageData(coords.x, coords.y, 1, 1).data
 }
 
 function draw(x, y) {
     if(drawing) {
-        let coords = get_canvas_coords(x, y)
-        
-        ctx.fillStyle = color
-        ctx.fillRect(coords.x, coords.y, 1, 1)
+        let coords  = get_canvas_coords(x, y)
+        let update  = ctx.createImageData(1, 1)
+
+        update.data[0] = color[0]
+        update.data[1] = color[1]
+        update.data[2] = color[2]
+        update.data[3] = color[3]
+
+        ctx.putImageData(update, coords.x, coords.y)
+    }
+}
+
+function resize(e) {
+    if(window.innerHeight / canvas.height > window.innerWidth / canvas.width) {
+        canvas.style.width  = window.innerWidth + "px"
+        canvas.style.height = "auto"
+    } else {
+        canvas.style.height = window.innerHeight + "px"
+        canvas.style.width  = "auto"
     }
 }
 
 
 
-onload = (e) => {
-    canvas  = document.getElementsByTagName("canvas")[0]
-    ctx     = canvas.getContext("2d")
-    
-    ctx.fillStyle = black
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-    
-    canvas.addEventListener("mousedown", (e) => {
-        drawing = true
-        
+function mousedown(e) {
+    e.preventDefault()
+
+    drawing = true
+
+    if(shifting) {
+        color = transparent
+    } else {
         let target = get_pixel(e.offsetX, e.offsetY)
 
         let is_black =  target[0] < 128
@@ -45,43 +61,60 @@ onload = (e) => {
 
         if(is_black)    { color = white }
         else            { color = black }
-        
-        draw(e.offsetX, e.offsetY)
-    })
-
-    canvas.addEventListener("mousemove", (e) => {
-        draw(e.offsetX, e.offsetY)
-    })
-
-    canvas.addEventListener("mouseup", (e) => {
-        draw(e.offsetX, e.offsetY)
-        drawing = false
-    })
-    
-    canvas.addEventListener("mouseleave", (e) => {
-        draw(e.offsetX, e.offsetY)
-        drawing = false
-    })
-}
-
-onresize = (e) => {
-    if(window.offsetHeight / canvas.height > window.offsetWidth / canvas.width) {
-        canvas.style.width  = "100vw"
-        canvas.style.height = "calc(" + canvas.height + "px * 100vh / " + canvas.width + "px)"
-    } else {
-        canvas.style.height = "100vh"
-        canvas.style.width  = "calc(" + canvas.width + "px * 100vw / " + canvas.height + "px)"
     }
+
+    draw(e.offsetX, e.offsetY)
+}
+
+function mousemove(e) {
+    e.preventDefault()
+
+    draw(e.offsetX, e.offsetY)
+}
+
+function mouseup(e) {
+    e.preventDefault()
+
+    draw(e.offsetX, e.offsetY)
+    drawing = false
 }
 
 
 
-document.addEventListener("keydown", (e) => {
+onload = e => {
+    canvas  = document.getElementsByTagName("canvas")[0]
+    ctx     = canvas.getContext("2d")
+
+    ctx.fillStyle = black
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    canvas.addEventListener("mousedown",    e => mousedown(e))
+    canvas.addEventListener("mousemove",    e => mousemove(e))
+    document.addEventListener("mouseup",    e => mouseup(e))
+
+    canvas.addEventListener("touchstart",   e => mousedown(e),  { passive: false })
+    canvas.addEventListener("touchmove",    e => mousemove(e),  { passive: false })
+    document.addEventListener("touchend",   e => mouseup(e),    { passive: false })
+
+    resize()
+}
+
+onresize = e => resize(e)
+
+
+
+document.addEventListener("keydown", e => {
     e.preventDefault()
-    //
+
+    switch(e.key) {
+        case "Shift": shifting = true
+    }
 })
 
-document.addEventListener("keyup", (e) => {
+document.addEventListener("keyup", e => {
     e.preventDefault()
-    //
+
+    switch(e.key) {
+        case "Shift": shifting = false
+    }
 })
